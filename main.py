4e29193c0c6d9580f0fd44a74433bc3f4e11bc0f
David Edwards <arkiados@bellsouth.net>
2023-07-03 12:02:10 -0700

    final commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import base64
 
 from flask import Flask, request
-from model import Message 
+from model import Message
 
 app = Flask(__name__)
 
@@ -25,18 +25,15 @@ def home():
 
 <h2>Wisdom From Your Fellow Classmates</h2>
 """
-    
     for m in Message.select():
         body += """
 <div class="message">
 {}
 </div>
-""".format(m.content)
+""".format(m.content.replace('<', '&lt;').replace('>', '&gt;'))
 
-    return body 
-
+    return body
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
